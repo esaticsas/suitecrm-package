244d6a017e9ec6c77a3a6de9c244d8b9478d32b8
Jonathan <jonathan.casas@esatic.com>
2023-02-20 14:38:05 -0500

    Add relation object to event for use in listeners

--- src/Events/RelatedItemsEvent.php
+++ src/Events/RelatedItemsEvent.php
@@ -8,16 +8,20 @@ use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Http\Request;
 use Illuminate\Queue\SerializesModels;
+use Esatic\Suitecrm\Models\RelatedModule;
 
-class RelatedItemsEvent
-{
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+class RelatedItemsEvent {
+
+    use Dispatchable,
+        InteractsWithSockets,
+        SerializesModels;
 
     protected Builder $query;
     protected Request $request;
     protected string $module;
     protected string $relationName;
     protected string $id;
+    protected RelatedModule $relateModule;
 
     /**
      * @param Builder $query
@@ -26,63 +30,64 @@ class RelatedItemsEvent
      * @param string $relationName
      * @param string $id
      */
-    public function __construct(Builder $query, Request $request, string $module, string $relationName, string $id)
-    {
+    public function __construct(Builder $query, Request $request, string $module, string $relationName, string $id, RelatedModule $relateModule) {
         $this->query = $query;
         $this->request = $request;
         $this->module = $module;
         $this->relationName = $relationName;
         $this->id = $id;
+        $this->relateModule = $relateModule;
     }
 
     /**
      * @return Builder
      */
-    public function getQuery(): Builder
-    {
+    public function getQuery(): Builder {
         return $this->query;
     }
 
     /**
      * @return Request
      */
-    public function getRequest(): Request
-    {
+    public function getRequest(): Request {
         return $this->request;
     }
 
     /**
      * @return string
      */
-    public function getModule(): string
-    {
+    public function getModule(): string {
         return $this->module;
     }
 
     /**
      * @return string
      */
-    public function getRelationName(): string
-    {
+    public function getRelationName(): string {
         return $this->relationName;
     }
 
     /**
      * @return string
      */
-    public function getId(): string
-    {
+    public function getId(): string {
         return $this->id;
     }
 
+    /**
+     * @return RelatedModule
+     */
+    public function getRelateModule(): RelatedModule {
+        return $this->relateModule;
+    }
 
     /**
      * Get the channels the event should broadcast on.
      *
      * @return \Illuminate\Broadcasting\Channel|array
      */
-    public function broadcastOn()
-    {
+    public function broadcastOn() {
         return new PrivateChannel('channel-name');
     }
+
 }
